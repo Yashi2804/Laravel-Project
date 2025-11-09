5722b1f8df95db6845f7901be01c80cc6e7e41aa
Avanish Rajput <avanishrajput208017@gmail.com>
2025-11-09 17:14:52 +0530

    09-11-2025

--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::get('/hello', function(){
-    return view('hello');
-});
 Route::get('/dash', function () {
     return view('LARAVEL_PROJECT.dashboards.main_dashboard');
 })->name('main-dash');
-Route::get('/index', function () {
+Route::get('/', function () {
     return view('LARAVEL_PROJECT.dashboards.index_dashboard');
 })->name('index-dash');
-
-
 Route::get('/admin',function(){
     return view('admin');
 });
